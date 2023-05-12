9fcb2611f25a3d2f744e7e5062c694a2e19a8cfa
Samuel Kiragu <samuelkiragu107@gmail.com>
2023-05-12 15:05:38 +0300

    Created Application skeleton

---
+++ UserPayment/views.py
@@ -0,0 +1,16 @@
+# Track the state of token
+# This will inform when to get another access token
+ACCESS_TOKEN = None
+TOKEN_EXPIRY_DATE = None
+NOTIFICATION_ID = None
+
+def get_access_token(consumer_key, consumer_secret):
+    return ""
+
+def register_IPN_URL(url, notification_type):
+    return ""
+
+def make_payment(first_name, last_name, 
+                 phone_number, email):
+    return ""
+
