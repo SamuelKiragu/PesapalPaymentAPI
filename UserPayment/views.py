13d7a9918a0b96a93e50a2e5eb165e3bfe3da01e
Samuel Kiragu <samuelkiragu107@gmail.com>
2023-05-13 19:25:01 +0300

    Added Function Skeleton View Name: Submit Order

--- UserPayment/views.py
+++ UserPayment/views.py
@@ -1,1 +1,19 @@
+from rest_framework import APIView
+from rest_framework.response import Response
+from rest_framework import authentication, permissions
+
+class SubmitOrder(APIView):
+    """
+    View to submit order
+    Makes API call to Pesapal
+    Returns a JSON object containing the iframe URL
+    """
+    
+    def post(self, request, format=None):
+        # TODO: Response skeleton
+        # TODO: Extract Data from request
+        # TODO: Add additional data
+        # TODO: Make API call to pesapal
+        # TODO: Format response
+        # TODO: return response
 
