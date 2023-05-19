39166b3098dfe7d6eec545e942e6a36ca936d064
Samuel Kiragu <samuelkiragu107@gmail.com>
2023-05-19 14:29:06 +0300

    Completed submit_order endpoint

--- UserPayment/views.py
+++ UserPayment/views.py
@@ -9,6 +9,7 @@ from rest_framework.permissions import IsAdminUser
 from rest_framework.views import APIView
 from UserPayment import pesapal
 from UserPayment.serializers import *
+import random
 
 class UserList(generics.ListCreateAPIView):
     queryset = User.objects.all()
@@ -44,14 +45,14 @@ class SubmitOrder(APIView):
         if not self.PESAPAL_TOKEN or pesapal.is_token_expired(PESAPAL_TOKEN):
             self.PESAPAL_TOKEN = pesapal.get_access_token()
         # Submit Order
-            pesapal.submit_order_request(
-                id=#TODO,
-                currency='KES',
-                amount=request.query_params.get('amount'),
-                description='Payment',
-                callback_url=#TODO
-                ipn_url=#TODO,
-                billing_address=#TODO,
-                token=self.PESAPAL_TOKEN["token"]
-            )
-        return Response(self.PESAPAL_TOKEN)
+        results = pesapal.submit_order_request(
+            id=random.randint(10000,99999),
+            currency='KES',
+            amount=request.query_params.get('amount'),
+            description='Payment',
+            callback_url='http://127.0.0.0:8000/',
+            ipn_url='http://127.0.0.0:8000/',
+            billing_address= UserSerializer(request.user).data,
+            token=self.PESAPAL_TOKEN["token"]
+        )
+        return Response(results)
