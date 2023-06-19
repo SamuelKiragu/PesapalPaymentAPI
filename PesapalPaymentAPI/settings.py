df41254cc969576c7973cc3b678f24da2257b9c3
Samuel Kiragu <samuelkiragu107@gmail.com>
2023-06-19 15:46:34 +0300

    Configured Simple JWT

--- PesapalPaymentAPI/settings.py
+++ PesapalPaymentAPI/settings.py
@@ -29,6 +29,9 @@ AUTHENTICATION_BACKENDS = (
 )
 
 REST_FRAMEWORK = {
+        'DEFAULT_AUTHENTICATION_CLASSES': (
+            'rest_framework_simplejwt.authentication.JWTAuthentication',
+        )
         'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
         'PAGE_SIZE': 10
 }
